32106a84b20cbc844fde4e262bf47d256c264141
omwokita <digiomuok>
2020-02-16 16:05:11 +0300

    added mailgum mailing functionality

--- user/includes/mailer.php
+++ user/includes/mailer.php
@@ -1,44 +1,19 @@
 <?php
- 
- 
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
+require './././vendor/autoload.php';
+use Mailgun\Mailgun;
 
-function sendmail($to, $subject, $message)
+function sendmail($subject,$to,$message)
 {
 
-        //Load Composer's autoloader
-    require 'vendor/autoload.php';
-    // Instantiation and passing `true` enables exceptions
-    $mail = new PHPMailer(true);
-
-    try {
-        //SMPT SERVER CONFIGURATION
-        // $mail->SMTPDebug = 2;    // Enable verbose debug output
-        $mail->isSMTP();         // Set maile0791342771KYUCU.r to use SMTP
-        $mail->Host = 'mail.kyucu.co.ke';
-        $mail->SMTPAuth   = true;
-        $mail->Username   = 'info@kyucu.co.ke';        // SMTP username
-        $mail->Password   = ');c4XVQdwtru';
-        $mail->SMTPSecure = 'ssl';     // Enable TLS encryption, `ssl` also accepted
-        $mail->Port       = 465;
-          
-
-            
-        //RECEPIENTS
-        $mail->setFrom('noreply@kyucu.co.ke', 'Christian Union - KYU');
-        $mail->addAddress($to);
-
-        
-        //CONTENT
-        $mail->isHTML(true);
-        $mail->Subject = $subject;
-        $mail->Body    = $message;
-
-        $mail->send();
-
-        echo 'message has been sent';
-    } catch (Exception $e) {
-        echo 'error while sending the message . Error:', $mail->ErrorInfo;
-    }
+    // First, instantiate the SDK with your API credentials
+    $mg = Mailgun::create('key-fa6dde58116f9566eebd52ad786685b5', 'https://api.mailgun.net/v3/kyucu.co.ke'); // For EU servers
+    
+    // Now, compose and send your message.
+    // $mg->messages()->send($domain, $params);
+    $mg->messages()->send('kyucu.co.ke', [
+      'from'    => 'KYUCU@kyucu.co.ke',
+      'to'      => $to,
+      'subject' => $subject,
+      'text'    => $message
+    ]);
 }
